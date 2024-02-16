196948ba7f239337a0600737f9d9fdabdb9fd7c2
Monalis2001 <159414514+Monalis2001@users.noreply.github.com>
2024-02-16 06:42:46 +0000

    1st commit

---
+++ 01-Basics/01-variable.js
@@ -0,0 +1,15 @@
+const accoundID = 13445
+let accountEmail = "web@gmail.com"
+var accountPassword = "123456"
+ accountCity = "bhonser"
+let accountState = "odisha"
+
+
+console.log(accoundID)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+console.log(accountState) // undefiend
+
+
+console.table([accoundID, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01-Basics/02-Datatypes.js
@@ -0,0 +1,40 @@
+// basically datatypes are 2 types.
+// primitive - string, number, bigint, null,undefined,symbol,boolean.
+
+
+  const score = 100 //number
+  const isLoggedIN = false //boolean
+  const respond = null //null
+  let serEmail; //undefined
+  var userState = "odisha" //string
+  const MonaBankBalance = 124354678790; //bright
+
+
+const id = Symbol('123'). // one character
+const anotherId = Symbol('124') // another character
+
+
+
+
+
+// nonprimitive or refrence
+// 1. array
+
+
+const monaCars = ["bmw", "audi", "maybeach","porche", "nano" ]
+
+//2. function
+ 
+const myFunc = function(){
+    console.log("hello world")
+}
+
+
+//3. object
+
+
+const myObj ={
+    name : "mona",
+    age : 78,
+    city : "puducheri"
+}
